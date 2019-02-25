db6424449c2b868f119f9444bdab83cadc1a75ab
dinhlong24 <dinhlong241@gmail.com>
2019-02-25 09:35:52 +0700

    update

--- scripts/scrollbar.js
+++ scripts/scrollbar.js
@@ -1,8 +1,9 @@
+
 const main = Scrollbar.init(app.select('.scroll-Animate'),{
-        damping: 0.11,
-        thumbMinSize: 36,
-        renderByPixels: false,
-        alwaysShowTracks: false,
-        continuousScrolling: false
-    }
-)
+            damping: 0.11,
+            thumbMinSize: 36,
+            renderByPixels: false,
+            alwaysShowTracks: false,
+            continuousScrolling: false
+        }
+    )
