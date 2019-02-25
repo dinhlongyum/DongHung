27af2fdec0d83ebdd7cd5ff649356d8723c5411d
dinhlongyum <dinhlong241@gmail.com>
2019-02-25 17:28:21 +0700

    update

--- scripts/scrollbar.js
+++ scripts/scrollbar.js
@@ -1,4 +1,5 @@
 
+
 const main = Scrollbar.init(app.select('.scroll-Animate'),{
             damping: 0.11,
             thumbMinSize: 36,
@@ -7,3 +8,14 @@ const main = Scrollbar.init(app.select('.scroll-Animate'),{
             continuousScrolling: false
         }
     )
+if(window.innerWidth < 992){
+    const headerMainNav = Scrollbar.init(app.select('.dh-header .main-nav'),{
+                damping: 0.11,
+                thumbMinSize: 36,
+                renderByPixels: false,
+                alwaysShowTracks: false,
+                continuousScrolling: false
+            }
+        )
+}
+
